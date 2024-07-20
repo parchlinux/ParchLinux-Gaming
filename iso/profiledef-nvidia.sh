46d66bf79e83a0d350d0566b2e07bcfef87921b8
Sohrab Behdani <behdanisohrab@gmail.com>
2024-07-20 11:00:20 +0330

    removed duaplicate line

--- iso/profiledef-nvidia.sh
+++ iso/profiledef-nvidia.sh
@@ -14,7 +14,6 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '8M' '-Xcompression-level' '9')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
